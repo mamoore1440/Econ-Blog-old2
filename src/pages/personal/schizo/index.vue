1e552610d79bdd0cd797b299b2a3625c3b3f5cc3
mamoore1440 <mamoore1440@gmail.com>
2022-08-18 12:04:00 -0500

    add index.vue for personal

---
+++ src/pages/personal/schizo/index.vue
@@ -0,0 +1,26 @@
+<template> 
+  <main>
+    <nav-bar current-page="Schizo" />
+    <blog-feed route='/personal/schizo'/>
+    <back-to-top-button />
+    <footer-bar current-page="Schizo" />
+  </main>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+import BlogFeed from '@/components/BlogFeed.vue';
+import FooterBar from '@/components/FooterBar.vue';
+import BackToTopButton from '@/components/BackToTopButton.vue';
+
+export default {
+  name: 'schizo',
+  components: {
+    NavBar,
+    BlogFeed,
+    BackToTopButton,
+    FooterBar
+  },
+};
+</script>
+<!-- Copy of blog.vue -->
